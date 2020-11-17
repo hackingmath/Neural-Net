dcd2f2aff2eea4d5c27e66881158702e345de30c
Peter Farrell <farrellpolymath@gmail.com>
2020-11-17 16:57:12 -0600

    Add files via upload

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -4,167 +4,179 @@ May 30, 2017'''
 
 import numpy as np
 import random
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from math import exp
-#import scipy.special
+
+
+# import scipy.special
 
 class NeuralNetwork(object):
 
-    #initialize the neural network
+    # initialize the neural network
     def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
         self.inodes = inputnodes
         self.hnodes = hiddennodes
         self.onodes = outputnodes
 
-        #link weight matrices, wih and who
-        #weights inside the arrays are w_i_j, where link
-        #is from node i to node j in the next layer
-        #w11 w21
-        #w12 w22 etc
+        # link weight matrices, wih and who
+        # weights inside the arrays are w_i_j, where link
+        # is from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc
         self.wih = np.random.normal(0.0, pow(self.hnodes, -0.5),
-                                       (self.hnodes, self.inodes))
+                                    (self.hnodes, self.inodes))
         self.who = np.random.normal(0.0, pow(self.onodes, -0.5),
-                                       (self.onodes, self.hnodes))
-        
-        #learning rate
+                                    (self.onodes, self.hnodes))
+        # self.net = [self.wih,self.who]
+        # learning rate
         self.lr = learningrate
         pass
 
-    def activation_function(self,x,sigm = True):
-        #activation function is the sigmoid function
-        out = np.zeros([len(x),1])
-        for i,element in enumerate(x):
+    def activation_function(self, x, sigm=True):
+        # activation function is the sigmoid function
+        '''for i,element in enumerate(x):
             #print("element: ",element)
             if sigm:
                 out[i] = sigmoid(x[i][0])
             else:
                 out[i] = relu(x[i][0])
-            #print(out)
-        return out
+            #print(out)'''
+        # this can be done much faster using numpy
+        out = sigmoid(x[:, 0]) if sigm else relu(x[:, 0])  # <><><><><><>#.#<><><><><><>#.#<><><><><><>#
+        return out.reshape(-1, 1)  # NB this needs to be 2D so it can be used for output as well
 
-    def copy(self):
-        output = NeuralNetwork(self.inodes,self.hnodes,self.onodes,self.lr)
-        output.wih = self.wih.copy()
-        output.who = self.who.copy()
+    def replicate(self):
+        output = NeuralNetwork(self.inodes, self.hnodes, self.onodes, self.lr)
+        output.wih = np.copy(self.wih)
+        output.who = np.copy(self.who)
         output.lr = self.lr
         return output
 
-    def mutate(self,rate = 0.3):
-        if random.random() < rate:
-            val = 1.0 + np.random.normal(0,0.01)
-        else:
-            val = 1.0
-        self.wih *= val
-        self.who *= val
+    def mutate(self, rate=0.5, amount=0.1):
+        flat_wih = self.wih.flatten()
+        for i, n in enumerate(flat_wih):
+            if random.random() < rate:
+                flat_wih[i] *= 1.0 + np.random.normal(0.0, amount)
+        self.wih = np.copy(flat_wih.reshape(self.hnodes, self.inodes))
+
+        flat_who = self.who.flatten()
+        for i, n in enumerate(flat_who):
+            if random.random() < rate:
+                flat_who[i] *= 1.0 + np.random.normal(0.0, amount)
+        self.who = np.copy(flat_who.reshape(self.onodes, self.hnodes))
         return self
 
-
-    #train the neural network
+    # train the neural network
     def train(self, inputs_list, targets_list):
-        #convert inputs list to 2d array
+        # convert inputs list to 2d array
         inputs = np.array(inputs_list, ndmin=2).T
         targets = np.array(targets_list, ndmin=2).T
 
-        #calculate signals into hidden layer
+        # calculate signals into hidden layer
         hidden_inputs = np.dot(self.wih, inputs)
-        #calculate the signals emerging from hidden layer
-        hidden_outputs = self.activation_function(hidden_inputs,False)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs, False)
 
-        #calculate signals entering final output layer
+        # calculate signals entering final output layer
         final_inputs = np.dot(self.who, hidden_outputs)
-        #calculate signals exiting final output layer
-        final_outputs = self.activation_function(final_inputs,False)
+        # calculate signals exiting final output layer
+        final_outputs = self.activation_function(final_inputs, False)
 
-        #output layer error is the (target - actual)
+        # output layer error is the (target - actual)
         output_errors = targets - final_outputs
-        #hidden layer error is the output_errors, split by weights,
-        #recombined at hidden nodes
+        # hidden layer error is the output_errors, split by weights,
+        # recombined at hidden nodes
         hidden_errors = np.dot(self.who.T, output_errors)
 
-        #update the weights for the links between the
-        #hidden and output layers
-        self.who += self.lr * np.dot((output_errors * final_outputs *\
+        # update the weights for the links between the
+        # hidden and output layers
+        self.who += self.lr * np.dot((output_errors * final_outputs * \
                                       (1.0 - final_outputs)),
                                      np.transpose(hidden_outputs))
 
-        #update the weights for the links between the input
-        #and hidden layers
-        self.wih += self.lr * np.dot((hidden_errors * hidden_outputs *\
+        # update the weights for the links between the input
+        # and hidden layers
+        self.wih += self.lr * np.dot((hidden_errors * hidden_outputs * \
                                       (1.0 - hidden_outputs)),
                                      np.transpose(inputs))
         pass
 
-    #query the neural network
+    # query the neural network
     def query(self, inputs_list):
-        #convert inputs list to 2d array
-        inputs = np.array(inputs_list,ndmin=2).T
+        """Forward propagation of an array of inputs
+        through the network to an array of outputs"""
+        # convert inputs list to 2d array
+        inputs = np.array(inputs_list, ndmin=2).T
 
-        #calculate signals into hidden layer
+        # calculate signals into hidden layer
         hidden_inputs = np.dot(self.wih, inputs)
-        #calculate the signals emerging from the
-        #hidden layer
-        #print("Hidden inputs:", hidden_inputs)
-        hidden_outputs = self.activation_function(hidden_inputs,False)
+        # calculate the signals emerging from the
+        # hidden layer
+        # print("Hidden inputs:", hidden_inputs)
+        hidden_outputs = self.activation_function(hidden_inputs, False)
 
-        #calculate signals into final output layer
+        # calculate signals into final output layer
         final_inputs = np.dot(self.who, hidden_outputs)
-        #calculate the signals emerging from final
-        #output layer
-        final_outputs = self.activation_function(final_inputs,False)
-        #print("final_outputs: ",final_outputs)
+        # calculate the signals emerging from final
+        # output layer
+        final_outputs = self.activation_function(final_inputs, False)
+        # print("final_outputs: ",final_outputs)
         return final_outputs
 
-def sigmoid(t):
-    return 1/(1+exp(-t))
 
-def relu(t):
-    return max(0,t)
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(x * -1.0))  # <><><><><><>#.#<><><><><><>#.#<><><><><><>#
+
+
+def relu(x):
+    return np.maximum(x, 0.0)  # <><><><><><>#.#<><><><><><>#.#<><><><><><>#
+
 
 def main():
-    #number of input, hidden and output nodes
+    # number of input, hidden and output nodes
     input_nodes = 784
     hidden_nodes = 100
     output_nodes = 10
 
-    #learning rate
+    # learning rate
     learning_rate = 0.3
 
-    #create instance of neural network
+    # create instance of neural network
     n = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
 
-    #load the mnist training data CSV file into a list
-    training_data_file = open('mnist_train_100.csv','r')
+    # load the mnist training data CSV file into a list
+    training_data_file = open('mnist_train_100.csv', 'r')
     training_data_list = training_data_file.readlines()
     training_data_file.close()
 
-    #train the neural network
-    #go through all records in training data set
+    # train the neural network
+    # go through all records in training data set
     for record in training_data_list:
-        #split the record by the commas
+        # split the record by the commas
         all_values = record.split(',')
-        #scale and shift the inputs
+        # scale and shift the inputs
         inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-        #create the target output values (all 0.01 except the
-        #desired label which is 0.99)
+        # create the target output values (all 0.01 except the
+        # desired label which is 0.99)
         targets = np.zeros(output_nodes) + 0.01
-        #print("all values[0]: ",all_values[0])
-        #print("targets: ",targets)
-        #all values[0] is the target label for this record
+        # print("all values[0]: ",all_values[0])
+        # print("targets: ",targets)
+        # all values[0] is the target label for this record
         targets[int(all_values[0])] = 0.99
-        n.train(inputs,targets)
+        n.train(inputs, targets)
         pass
 
-    #load the mnist data csv file into a list
-    test_data_file = open('mnist_test_10.csv','r')
+    # load the mnist data csv file into a list
+    test_data_file = open('mnist_test_10.csv', 'r')
     test_data_list = test_data_file.readlines()
     test_data_file.close()
 
     ###TO TEST###
-    #get the first test record
+    # get the first test record
     all_values = test_data_list[0].split(',')
-    #print the label
+    # print the label
     print(all_values[0])
-    image_array = np.asfarray(all_values[1:]).reshape((28,28))
+    image_array = np.asfarray(all_values[1:]).reshape((28, 28))
     plt.imshow(image_array, cmap="Greys", interpolation='None')
     plt.show()
 
@@ -175,17 +187,29 @@ def main():
     a[0,1] = 2
     a[1,0] = 9
     a[2,1] = 12
-    
+
     plt.imshow(a, interpolation = 'nearest')
     plt.show()'''
 
-    #print(n.query([1.0, 0.5, -1.5]))
+    # print(n.query([1.0, 0.5, -1.5]))
+
+
+def test():
+    brain = NeuralNetwork(2, 3, 1, 0.8)
+    print("brain:", brain.wih)
+    brain2 = brain.replicate()
+    print('brain2:', brain2.wih)
+    brain2.mutate()
+    print("brain2 mutated:", brain2.wih)
+    # a = np.random.normal(0.0, pow(3, -0.5),
+    #                                    (3,2))
+    # print(a)
+    #
+    # a *= np.random.random()
+    # print(a)
+    # b = np.copy(a)
+    # print(b)
+
 
 if __name__ == "__main__":
-    #main()
-    n = NeuralNetwork(2, 4, 2, 0.3)
-    print(n.wih)
-    m = n.copy()
-    print(n.wih)
-    print(m.wih)
-
+    main()
---
+++ ship_ai_PF.py
@@ -0,0 +1,281 @@
+import pygame
+import time
+from math import pi, sqrt, cos, sin, atan2,copysign
+import neuralNetwork as nn
+import polar_cartesian as pc
+import numpy
+from asteroid import Asteroid
+from bullet import Bullet
+
+# define constants
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+CYAN = (0, 255, 255)
+VIOLET = (148, 0, 211)
+
+width,height = 600,600
+asteroid_count = 10
+GRAPHICS_ON = False
+AI_PLAYING = True
+
+# Create function to draw texts
+def drawText(msg, color, x, y, s, center=True):
+    screen_text = pygame.font.SysFont("Calibri", s).render(msg, True, color)
+    if center:
+        rect = screen_text.get_rect()
+        rect.center = (int(x), int(y))
+    else:
+        rect = (int(x), int(y))
+    screen.blit(screen_text, rect)
+
+class Ship():
+    def __init__(self,size=50,width=600,height=600,graphics=True,brain=None):
+        self.size = size
+        self.rotation_angle = 0
+        self.x, self.y = width / 2, height / 2
+        vertices = [(0.5,0),(-0.25,-0.25),(-0.25,0.25)]
+        #convert x-y vertices to polar
+        self.points = [pc.to_polar(v) for v in vertices]
+        #scale up ship by self.size
+        self.points = [[self.size*r,theta] for [r,theta] in self.points]
+        self.color = GREEN
+        self.score = 0 #game score
+        self.alive = True
+        self.life = 0 #number of frames survived
+        self.dpoints = [0]*8 #[[0,0,0]] * 8 #points for testing distance measurements
+        self.create_asteroids = False
+        self.shoot_bullet = False #True when key pressed or AI chooses
+        self.createBullet = True #Off when bullet has been created, creates delay
+        self.bullet_wait = 0
+        self.graphics = graphics
+        self.fitness = 0
+        #for neural net
+        if brain:
+            self.brain = brain.replicate()
+        else:
+            self.brain = nn.NeuralNetwork(8, 12, 3, 0.3)
+        self.inputs = [0] * 5
+        self.output = numpy.zeros(3)
+        #for tracking improvements
+        self.mutated = 0
+        self.crossovers = 0
+
+    def update(self,asteroids,asteroid_count):
+        self.life += 1
+        for a in asteroids:
+            if pc.distance(self,a) < a.size:
+                self.alive = False
+                return
+        #if shooting, wait before creating another bullet
+        if self.createBullet == False and time.time() - self.bullet_wait >= 0.2:
+            self.createBullet = True
+
+    def draw(self):
+        #rotate vertices,convert to x-y format
+        cartesian_points = [pc.to_cartesian([r,theta+self.rotation_angle]) for [r,theta] in self.points]
+        self.screen_points = [[int(self.x + pt[0]),int(self.y+pt[1])] for pt in cartesian_points]
+
+    def get_score(self):
+        return self.score
+
+    def check_asteroids(self,x,y,asteroids):
+        for a in asteroids:
+            if pc.distance_pts(x, y, a.x, a.y) < a.size:
+
+                return ((self.x - x)**2+(self.y - y)**2)**0.5
+
+    def measure(self,asteroids):
+        """Draw lines radiating outward to measure distances to objects"""
+        #Orland's method:
+        # for i in range(8):
+        #     dist = 600
+        #     closest = 600
+        #     ang = self.rotation_angle + 2*pi*i/8
+        #     c,s = cos(ang),sin(ang)
+        #     segment = ((self.x,self.y),(dist*c,dist*s))
+        #     for ast in asteroids:
+        #         d = ast.does_intersect(segment) #if it intersects, d is distance
+        #         print("d:",d)
+        #         if d:
+        #             if d < closest:
+        #                 closest = d
+        #     self.dpoints[i] = closest
+        # print(self.dpoints)
+
+        #my method:
+        for i in range(8):
+            dist = 0
+            ang = self.rotation_angle + 2*pi*i/8
+            c,s = cos(ang),sin(ang)
+            while dist < 500:
+                dist += 1
+                x,y = self.x + dist*c, self.y + dist*s
+                intersect = self.check_asteroids(x,y,asteroids)
+                if intersect:
+                    self.dpoints[i] = intersect
+                    #print("intersect")
+                    break
+            #PG's suggestion:
+            # self.dpoints[i][2] = 99999
+            # for a in asteroids:
+            #     (dx, dy) = (a.x - self.x, a.y - self.y)
+            #     nearness = abs(c * a.x - s * a.y + s * self.x - c * self.y)
+            #     # need to also check it's in the right quadrant as line goes in both directions!
+            #     if c == copysign(c, dx) and s == copysign(s, dy) and nearness < a.size:
+            #         dist = (dx ** 2 + dy ** 2) ** 0.5 - a.size  # pretty approximately!
+            #         if dist < self.dpoints[i][2]:  # only if this point is nearer
+            #             self.dpoints[i] = [self.x + c * dist, self.y + s * dist, dist]
+
+    def left(self):
+        """AI turns left"""
+        self.rotation_angle -= 0.1
+        #print("Left")
+
+    def right(self):
+        """AI turns right"""
+        self.rotation_angle += 0.1
+        #print("Right")
+
+    def shoot(self,bulletList):
+        """AI shoots bullet"""
+        #print("Shoot")
+        self.shoot_bullet = True
+
+    def think(self,bulletList):
+        #
+        self.inputs = numpy.zeros(8)
+
+        for j,pt in enumerate(self.dpoints):
+            #if pt[0] != 0 and pt[1] != 0:
+            self.inputs[j] = pt#pc.sq_dist(self,pt)#pt[2]#
+        self.output = self.brain.query(numpy.array(self.inputs))  #
+        if numpy.argmax(self.output) == 0:  # left
+            self.left()
+        elif numpy.argmax(self.output) == 1:  # right
+            self.right()
+        elif numpy.argmax(self.output) == 2:
+            self.shoot(bulletList)
+
+    def calcFitness(self,ships):
+        total = sum([ship.get_score() for ship in ships])
+        if total > 0:
+            self.fitness = self.get_score() + self.life / total
+
+    def play(self):
+        global asteroid_count
+
+        if GRAPHICS_ON:
+            # set up display
+            pygame.init()
+
+            screen = pygame.display.set_mode([width, height])
+
+            #in case you use fonts:
+            pygame.font.init()
+            myfont = pygame.font.SysFont('Consolas', 24)
+            scorefont = pygame.font.SysFont('Consolas', 72)
+
+            pygame.display.set_caption('Pygame Window') #add your own caption!
+
+            FPS = 60  # frames per second
+            clock = pygame.time.Clock()
+
+        counter = 0 #frame count
+
+        # loop until user clicks the close button
+        done = False
+
+        #create the objects
+        # ship = Ship(50,width,height,True)
+        bullets = []
+        asteroids = [Asteroid() for _ in range(asteroid_count)]
+
+        while self.alive and not done:
+            if GRAPHICS_ON:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:  # if pygame window is closed by user
+                        done = True
+            if not AI_PLAYING:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_LEFT]:
+                    self.rotation_angle -= 0.1
+                if keys[pygame.K_RIGHT]:
+                    self.rotation_angle += 0.1
+                if keys[pygame.K_SPACE]:
+                    self.shoot(bullets)
+
+                if keys[pygame.K_p]:
+                    if FPS == 60:
+                        FPS = 300  #faster display
+                    else:
+                        FPS = 60
+            if GRAPHICS_ON:
+                # fill the screen with background color
+                screen.fill(BLACK)
+
+            counter += 1
+            if AI_PLAYING:
+                self.think(bullets)
+
+            self.update(asteroids,asteroid_count)
+            if len(bullets) < 4 and self.createBullet and self.shoot_bullet:
+                bullets.append(Bullet(self.x, self.y, self.rotation_angle,GRAPHICS_ON))
+                self.shoot_bullet = False
+                self.createBullet = False
+                self.bullet_wait = time.time()
+            self.measure(asteroids)
+            if self.create_asteroids:
+                asteroids = [Asteroid() for _ in range(asteroid_count)]
+            if GRAPHICS_ON:
+                #print(self.dpoints)
+                for i,pt in enumerate(self.dpoints):
+                    #if pt[0] != 0 and pt[1] != 0:
+                    #pygame.draw.line(screen,BLUE,(int(pt[0]),int(pt[1])), (int(self.x), int(self.y)),2)
+                    pygame.draw.line(screen, BLUE, (int(self.x+pt*cos(self.rotation_angle+i*pi/4)),
+                                                    int(self.y+pt*sin(self.rotation_angle+i*pi/4))), (int(self.x), int(self.y)), 2)
+
+            #draw ship:
+            if GRAPHICS_ON:
+                self.draw()
+                pygame.draw.lines(screen, self.color, True, self.screen_points, 2)
+
+            if len(asteroids) == 0:
+                time.sleep(2)
+                asteroid_count += 2
+                asteroids = [Asteroid() for i in range(asteroid_count)]
+            for a in asteroids:
+                a.move()
+                a.check_bullets(self,asteroids,bullets)
+                if GRAPHICS_ON:
+                    #draw asteroids
+                    pygame.draw.lines(screen, GREEN, True, a.screen_points, 2)
+                    #a.draw()
+
+            for b in bullets:
+                b.update()
+                if GRAPHICS_ON:
+                    pygame.draw.circle(screen, GREEN, (int(b.x), int(b.y)), 3)
+                if b.life <= 0:
+                    try:
+                        bullets.remove(b)
+                    except:
+                        pass
+
+
+
+            #print()
+            # for saving screenshots:
+            # if counter %5 == 0:
+            # Capture(screen, 'Capture{}.png'.format(counter), (0, 0), (600, 600))
+
+            if GRAPHICS_ON:
+                clock.tick(FPS)
+                pygame.display.update()
+                pygame.quit()
+        return self.score + self.life
+
+s = Ship(50,600,600,True)
+s.play()
